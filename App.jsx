022a15f991bb7dcfd8a7f034b60df83cf7b68dda
bankarom <146808698+bankarom@users.noreply.github.com>
2024-02-25 15:55:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import HigherOrderComponents from './Components/HigherOrderComponents'
+
+function App() {
+  return(
+    <div className='App'>
+      <HigherOrderComponents/>
+    </div>
+  )
+}
+
+export default App
---
+++ HigherOrderComponents.jsx
@@ -0,0 +1,79 @@
+import { Component } from "react";
+
+export default class HigherOrderComponents extends Component{
+    constructor(){
+        super();
+        this.state = {
+            userData: [
+                { id: '1', name: 'Joe', user_type: 'Developer', age:31, years:11 },
+                { id: '2', name: 'Hill', user_type: 'Designer', age:26, years:4 },
+                { id: '3', name: 'John', user_type: 'Teacher', age:24, years: 2},
+                { id: '4', name: 'Sam', user_type: 'Entreprenuer', age:58,years:25},
+                { id: '5', name: 'Jack', user_type: 'Designer', age:43, years: 18}
+
+            ],
+            filteredData:[],
+            filteredWithName:[],
+            filteredWithAge:[],
+            total:0
+
+        }
+        
+        var filteredData = this.state.userData.filter((element)=>element.user_type === "Designer")
+        var filteredWithName = this.state.userData.filter((element)=>element.name[0]=="J")
+        var filteredWithAge = this.state.userData.filter((element)=>element.age>28 && element.age<=50)
+        var sum = this.state.userData.reduce((accumulator, element)=>{
+            if (element.user_type == "Designer"){
+                accumulator +=element.years;
+            }
+            return accumulator
+        }, 0);
+
+        this.state.filteredWithName = filteredWithName;
+        this.state.filteredData = filteredData;
+        this.state.filteredWithAge = filteredWithAge;
+        this.state.total = sum;
+
+    }
+
+    render(){
+        return(
+            <div style={{width:"80%", textAlign : "initial"}}>
+                <h1>Display All Items</h1>
+                {this.state.userData.map((element)=>(
+                    <div key={element.id} style={{width:"70%"}}>
+                        <ul>
+                            <li>ID: {element.id} Name : {element.name} Type: {element.user_type}</li>
+                        </ul>
+                    </div>
+                ))}
+
+                <h1>Display all data based on UserTyper</h1>
+                {this.state.filteredData.map((element)=>(
+                    <div key={element.id} style={{width:"70%"}}>
+                        <ul>
+                            <li>ID: {element.id} Name : {element.name} Type: {element.user_type}</li>
+                        </ul>
+                    </div>
+
+                ))}
+
+                <h1>Filtered all the data based on greater than 28 and less than 50</h1>
+                {this.state.filteredData.map((element)=>(
+                    <div key={element.id} style={{width:"70%"}}>
+                        <ul>
+                            <li>ID: {element.id} Name : {element.name} Type: {element.user_type}</li>
+                        </ul>
+                    </div>
+
+                ))}
+
+                <h1>Find the total years of Designers</h1>
+                <h2>{this.state.total}</h2>
+
+                
+            </div>
+        )
+    }
+
+}
